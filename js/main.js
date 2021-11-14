7e5672de938624ae27ca2571ec5089fa05beab9e
moamel hashim <moamel1997@gmail.com>
2021-11-14 01:19:04 +0000

    completed first feature

--- js/main.js
+++ js/main.js
@@ -2,25 +2,18 @@
 var $emoji = document.querySelector('.emoji');
 $emoji.addEventListener('click', emojiHandler);
 
-var showOrHide = true;
-
 function emojiHandler(event) {
-  if (showOrHide === true) {
-    var $logoRow = document.querySelector('.center-emoji');
-    $logoRow.className = 'hidden';
-    showOrHide = false;
-  }
-  if (showOrHide === false) {
-    var $header = document.querySelector('.under-line');
-    $header.className = 'under-line view';
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://icanhazdadjoke.com/');
-    xhr.responseType = 'json';
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.addEventListener('load', function () {
-      var $p = document.querySelector('p');
-      $p.textContent = xhr.response.joke;
-    });
-    xhr.send();
-  }
+  var $logoRow = document.querySelector('.center-emoji');
+  $logoRow.className = 'hidden';
+  var $header = document.querySelector('.under-line');
+  $header.className = 'under-line view';
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://icanhazdadjoke.com/');
+  xhr.responseType = 'json';
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.addEventListener('load', function () {
+    var $p = document.querySelector('p');
+    $p.textContent = xhr.response.joke;
+  });
+  xhr.send();
 }
