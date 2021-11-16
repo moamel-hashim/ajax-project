b46b6994c0221de86253bfebfd6afd8479058151
moamel hashim <moamel1997@gmail.com>
2021-11-16 01:33:16 +0000

    removed non-functional code

--- js/main.js
+++ js/main.js
@@ -14,17 +14,13 @@ function emojiHandler(event) {
   xhr.addEventListener('load', function () {
     var $p = document.querySelector('p');
     $p.textContent = xhr.response.joke;
-    $leftArrow.className = 'fas fa-chevron-left';
     $rightArrow.className = 'fas fa-chevron-right';
   });
   xhr.send();
 }
 
-var count = 0;
-var $leftArrow = document.querySelector('.fa-chevron-left');
 var $rightArrow = document.querySelector('.fa-chevron-right');
 $rightArrow.addEventListener('click', rightArrowEventHandler);
-$leftArrow.addEventListener('click', leftArrowEventHandler);
 
 function rightArrowEventHandler() {
   const xhr = new XMLHttpRequest();
