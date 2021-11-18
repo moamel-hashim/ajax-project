43511c8cf74fa32353832b33c1858db9cda13492
moamel hashim <moamel1997@gmail.com>
2021-11-18 03:27:30 +0000

    completed a feature user can view new dad joke and go back to a previous dad joke

--- js/main.js
+++ js/main.js
@@ -16,35 +16,42 @@ function emojiHandler(event) {
     $p.textContent = xhr.response.joke;
     $leftArrow.className = 'fas fa-chevron-left';
     $rightArrow.className = 'fas fa-chevron-right';
+    dadJokesArray.push(xhr.response.joke);
   });
   xhr.send();
 }
 
 var dadJokesArray = [];
-var count = 0;
+var dadJokesIndex = 0;
 var $leftArrow = document.querySelector('.fa-chevron-left');
 var $rightArrow = document.querySelector('.fa-chevron-right');
 $rightArrow.addEventListener('click', rightArrowEventHandler);
-// $leftArrow.addEventListener('click', leftArrowEventHandler);
+$leftArrow.addEventListener('click', leftArrowEventHandler);
 
 function rightArrowEventHandler(event) {
-  count++;
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://icanhazdadjoke.com/');
-  xhr.responseType = 'json';
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.addEventListener('load', function () {
-    var $p = document.querySelector('p');
-    $p.textContent = xhr.response.joke;
-    // var dadJokes = xhr.response.joke;
-    // dadJokesArray.push(dadJokes);
+  dadJokesIndex++;
+  if (dadJokesArray[dadJokesIndex] === undefined) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://icanhazdadjoke.com/');
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.addEventListener('load', function () {
+      var $p = document.querySelector('p');
+      $p.textContent = xhr.response.joke;
+      dadJokesArray.push(xhr.response.joke);
+    });
     xhr.send();
-  });
+  } else {
+    var $p = document.querySelector('p');
+    $p.textContent = dadJokesArray[dadJokesIndex];
+  }
+
 }
 
-// function leftArrowEventHandler(event) {
-//   for (var i = 0; i < dadJokesArray.length; i++) {
-//     // count--;
-
-//   }
-// }
+function leftArrowEventHandler(event) {
+  if (dadJokesIndex > 0) {
+    dadJokesIndex--;
+    var $p = document.querySelector('p');
+    $p.textContent = dadJokesArray[dadJokesIndex];
+  }
+}
